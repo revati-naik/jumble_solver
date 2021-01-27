4562c91aaa98accc9eb19307f1feb765f9091933
revati-naik <revatin@terpmail.umd.edu>
2021-01-27 17:41:19 -0500

    Adds search for substring. Takes cli input for filename. Handles url downlaod check.

--- Code/jumble_solver.py
+++ Code/jumble_solver.py
@@ -1,17 +1,19 @@
+from __future__ import print_function
+
 import urllib
 import os.path
 import argparse
 
 from urllib.request import urlopen
 
+
 ##
 ##Downloads the word list as data set
 ##
-def download_data():
+def download_data(data):
 
 	#link to the words list dataset
-	link = "http://www.mit.edu/~ecprice/wordlist.10000"
-	data = urlopen(link)
+	
 	words_data = data.read().decode('utf-8')
 
 	#file path to data set (.txt) file
@@ -27,6 +29,7 @@ def download_data():
 	else:
 		print("Data already exists.All set!")
 
+	
 ##
 ## Searchs for the word in the dataset
 ##
@@ -43,27 +46,86 @@ def search_word(search_word):
 	for word in file_data:
 
 		#check if the word is found
-		if word == search_word:
-			print("Original Word Found at ", i)
+		if sorted(word) == sorted(search_word):
+			print("Original Word Found: ", word)
 			# return
 
 		#check if the reverse word is found
-		if word == search_word[::-1]:
-			print("Reverse Word Found: ", search_word[::-1])
+		# if word == search_word[::-1]:
+		# 	print("Reverse Word Found: ", search_word[::-1])
+
+		search_substring(search_word=search_word, word=word)
 
 		i += 1
+
+def create_aux(word):
+    aux = [0] * len(word)
+
+    
+    i = 1
+    
+    j = 0
+    while i < len(word):
+        if word[i] == word[j]:
+            j += 1
+            aux[i] = j
+            i += 1
+        
+        elif word[i] != word[j] and j != 0:
+            j = aux[j-1]
+        else:
+             to the suffix for index i
+            aux[i] = 0
+            i += 1
+
+    return aux
+
+def search_substring(search_word, word):
+	aux = aux(word)
+
+	
+	i = 0
+	
+	j = 0
+	while j < len(search_word):
+	    if word[i] != search_word[j]:
+	        if i == 0:
+	            j += 1
+	        else:
+	            y will match anyway, that’s what kmp is about.
+	            i = aux[i-1]
+	    else:
+	        i += 1
+	        j += 1
+	        if i == len(word):
+	            print("Found substring in word list: ", word)
+	            i = aux[i-1]
+
 
 if __name__ == '__main__':
 
 	#download the data and save it in a text file
-	download_data()
+	link = "http://www.mit.edu/~ecprice/wordlist.10000"
+	try:
+		data = urlopen(link)
+		download_data(data=data)
 	
 
-	#read word input from the cli
-	parser = argparse.ArgumentParser()
-	parser.add_argument('word')
-	args = parser.parse_args()
-	print("Word to search for: ", args.word)
+		#read word input from the cli
+		parser = argparse.ArgumentParser()
+		# parser.add_argument('word')
+		# parser.add_argument('filename', )
+		parser.add_argument('-f', '--filename', help='Word Filename',required=False)
+		parser.add_argument('-w', '--word', help='Word', required=True)
+		args = parser.parse_args()
 
-	search_word(args.word)
-	
+
+		print("======================================")
+		print("Word List filename: ", args.filename)
+		print("Word to search for: ", args.word)
+		print("======================================")
+
+		search_word(args.word)
+	except:
+		print("Problem connecting to the url! Try Again")
+
