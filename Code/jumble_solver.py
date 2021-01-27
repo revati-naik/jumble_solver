ab8d71bac2faea0bde32b0b14caf5066b4c30497
revati-naik <revatin@terpmail.umd.edu>
2021-01-26 19:53:05 -0500

    Searches direct words in the dataset

--- Code/jumble_solver.py
+++ Code/jumble_solver.py
@@ -25,16 +25,30 @@ def download_data():
 	else:
 		print("Data already exists.All set!")
 
+def search_word(search_word):
+
+	#open the word list file and read it
+	file = open("./../Data/word_list.txt", "r")
+	file_data = file.read().split()
+	i = 0
+
+	for word in file_data:
+		if word == search_word:
+			print("Word Found! at ", i)
+			return
+		i += 1
+
 if __name__ == '__main__':
 
+	#download the data and save it in a text file
+	download_data()
 	
 
-	download_data()
-	file = open("./../Data/word_list.txt", "r")
-	file_data = file.read()
-
+	#read word input from the cli
 	parser = argparse.ArgumentParser()
 	parser.add_argument('word')
 	args = parser.parse_args()
 	print("Word to search for: ", args.word)
+
+	search_word(args.word)
 	
