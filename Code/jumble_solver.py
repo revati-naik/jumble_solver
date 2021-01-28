4e7d608569d3d774d30cea5f393c2ed366994d1b
revati-naik <revatin@terpmail.umd.edu>
2021-01-28 12:41:51 -0500

    Adds License

--- Code/jumble_solver.py
+++ Code/jumble_solver.py
@@ -1,3 +1,35 @@
+# MIT License
+
+# Copyright (c) 2021 Revati Naik
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# @file    jumble_solver.py
+# @author  Revati Naik
+# @version 1.0
+# @brief JUmble Solver (word match) implementation
+#
+# @section DESCRIPTION
+#
+#  python implementation for jumble solver which searches all the matches for the word
+#  (string) and the sub string in a list of english words
+
+
 from __future__ import print_function
 
 import sys
