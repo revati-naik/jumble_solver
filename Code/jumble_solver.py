43c281590117beb7a8b7053d7467d1ef468f934c
revati-naik <revatin@terpmail.umd.edu>
2021-01-28 11:57:42 -0500

    Adds commenting. Clears PEP8.

--- Code/jumble_solver.py
+++ Code/jumble_solver.py
@@ -1,161 +1,204 @@
 from __future__ import print_function
 
+import sys
 import os.path
 import argparse
 import requests
 
-##
-## Downloads the word list as data set
-##
+
 def download():
+    """
+    Downloads the word list as dataset
+    """
+    # link to the words list dataset
+    link = "http://www.mit.edu/~ecprice/wordlist.10000"
 
-	#link to the words list dataset
-	link = "http://www.mit.edu/~ecprice/wordlist.10000"
+    # try to connect to the specified url. Throw an exception on failure
+    try:
 
-	#try to connect to the specified url. Throw an exception on failure
-	try:
+        # mopen the link and read the data on the link
+        response = requests.get(link)
+        words_data = response.text
 
-		#open the link and read the data on the link
-		# data = urlopen(link)
-		response = requests.get(link)
-		words_data = response.text
+    # failed to connect to the url
+    except requests.exceptions.ConnectionError as e:
+        print("Problem connecting to the url! Try Again")
+        sys.exit()
+
+    try:
+        # file path to data set (.txt) file
+        data_file_path = "./../Data/word_list.txt"
+
+        # checks if the data set file exists
+        # if not true, will create a new file
+        if not os.path.isfile(data_file_path):
+            print("Downloading Dataset")
+
+            # opening the file
+            file = open("./../Data/word_list.txt", "w+")
+
+            # writing the downloaded data to the file
+            file.write(words_data)
+            print("Dataset Downloaded Successfully")
+
+        # if data file already exists
+        else:
+            print("Data already exists.All set!")
+
+    # Data folder does not exist
+    except FileNotFoundError as e:
+        print("Could not open the folder (/Data) to \
+            save the word list document")
+        sys.exit()
 
 
-		#file path to data set (.txt) file
-		data_file_path = "./../Data/word_list.txt"
-
-		#Checks if the data set file exists. If not true, will create a new file
-		if not os.path.isfile(data_file_path):
-			print("Downloading Dataset")
-
-			#opening the file 
-			file = open("./../Data/word_list.txt", "w+")
-			# f.seek(0)
-
-			#writing the downloaded data to the file
-			file.write(words_data)
-			# file.truncate()
-			print("Dataset Downloaded Successfully")
-
-		#if data file already exists
-		else:
-			print("Data already exists.All set!")
-
-	#failed to connect to the url
-	except:
-		print("Problem connecting to the url! Try Again")
-		exit()
-
-	
-##
-## Searchs for the word in the dataset
-##
-## :param      search_word:  The user deifned word for which we find the match
-## :type       search_word:  string
-## :param      filename:     The filename which contains a list of english words
-## :type       filename:     string
-##
 def search_word(search_word, file_path='word_list.txt'):
+    """
+    Searchs for the word in the word list
 
-	#try to open the file. Throws an exception if file does not exist
+    :param      search_word:  The user deifned word for which we find the match
+    :type       search_word:  { string }
+    :param      file_path:    The filename which contains a
+                              list of english words
+    :type       file_path:    { string}
+    """
 
-	if file_path == 'word_list.txt':
-		dir_path = './../Data/word_list.txt'
-	else:
-		dir_path = file_path
+    # try to open the file. Throws an exception if file does not exist
 
-	try:
-		#open the word list file and read it
-		file = open(dir_path, "r")
-		file_data = file.read().split()
-	except:
-		print("File does not exist!")
-		exit()
+    if file_path == 'word_list.txt':
+        dir_path = './../Data/word_list.txt'
+    else:
+        dir_path = file_path
 
+    try:
+        # open the word list file and read it
+        file = open(dir_path, "r")
+        file_data = file.read().split()
 
-	#iterating over every word in the file
-	for word in file_data:
+    except FileNotFoundError as e:
+        print("File does not exist!")
+        sys.exit()
 
-		#check if the search word matches the word word in the file. This also #check for anagrams
-		if sorted(word) == sorted(search_word):
-			print(word)
+    # iterating over every word in the file
+    for word in file_data:
 
-		#tries to search for substring matches
-		search_substring(search_word=search_word, word=word)
+        # check if the search word matches the word word in the file
+        # checks for anagrams
+        if sorted(word) == sorted(search_word):
+
+            # print the word from the list
+            print(word)
+
+        # tries to search for substring matches
+        search_substring(search_word=search_word, word=word)
+
 
 def create_aux(word):
+    """
+    Creates an auxiliary array
+
+    :param      word:  The word from the word list
+    :type       word:  { string }
+
+    :returns:   { Auxiliary array }
+    :rtype:     { list }
+    """
+
+    # initializing the aux array of lenth equal to the substring length
     aux = [0] * len(word)
 
+    # at index 0, there is no duplicate letter, hence that would always be 0,
+    # so we start from index 1
     i = 1
+
+    # index of the first mismatch
     j = 0
     while i < len(word):
+
+        # when there is a match, prefix = sufix[j-1]
         if word[i] == word[j]:
             j += 1
             aux[i] = j
             i += 1
-        
+        # when there is a mismatch, we check index of previous possible prefix
         elif word[i] != word[j] and j != 0:
             j = aux[j-1]
+        # when no prefix is found which is euqal to suffix for index i
         else:
             aux[i] = 0
             i += 1
 
     return aux
 
-def search_substring(search_word, word):
-	aux = create_aux(word)
 
-	i = 0
-	j = 0
-	while j < len(search_word):
-	    if word[i] != search_word[j]:
-	        if i == 0:
-	            j += 1
-	        else:
-	            i = aux[i-1]
-	    else:
-	        i += 1
-	        j += 1
-	        if i == len(word):
-	            print(word)
-	            i = aux[i-1]
+def search_substring(search_word, word):
+    """
+    Searches for the substring match in the word list
+
+    :param      search_word:  The user deifned word for which we find the match
+    :type       search_word:  { string }
+    :param      word:         The word from the word list
+    :type       word:         { string }
+    """
+
+    # creates the aux array for the word
+    aux = create_aux(word)
+
+    # counter for word from the word list (substring)
+    i = 0
+
+    # counter for search word (string)
+    j = 0
+
+    while j < len(search_word):
+
+        # mismatch condition
+        if word[i] != search_word[j]:
+
+            if i == 0:
+                # start next matching from the immediate next
+                # letter in the word string
+                j += 1
+            else:
+                # start comparing from aux[i-1], as word[0,..,aux[i-1]-1]
+                # already matches
+                i = aux[i-1]
+        else:
+            i += 1
+            j += 1
+
+            # match pattern found
+            if i == len(word):
+
+                # prints the match word found from the word list
+                print(word)
+
+                # continues to find more matches in the word
+                i = aux[i-1]
 
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
-	# download_data()
+    parser = argparse.ArgumentParser()
 
-	#read the filename from the user. This is an optional argument 
-	#deafult is the already available dataset in ./Data folder
-	parser.add_argument('-f', '--filename', help='Word List Filename', required=False, default='word_list.txt')
+    # read the filename from the user. This is an optional argument
+    # deafult is the already available dataset in ./Data folder
+    parser.add_argument('-f', '--filename', help='Word List Filename',
+                        required=False, default='word_list.txt')
 
-	#read the user defined word from the cli
-	parser.add_argument('-w', '--word', help='Search Word', required=True)
+    # read the user defined word from the cli
+    parser.add_argument('-w', '--word', help='Search Word', required=True)
 
-	#read the user input to make a choice on downloading data from the url
-	# parser.add_argument('-d', '--download_data', help='Download the data from the link', required=False)
+    args = parser.parse_args()
 
-	args = parser.parse_args()
+    # user selects to download the dataset
+    if args.filename == 'word_list.txt':
+        download()
 
-	#user selects to download the dataset
-	if args.filename == 'word_list.txt':
-		download()
+    print("======================================")
+    print("Word List filename: ", args.filename)
+    print("Word to search for: ", args.word)
+    print("======================================")
 
-	print("======================================")
-	print("Word List filename: ", args.filename)
-	print("Word to search for: ", args.word)
-	print("======================================")
-
-
-	# download the data and save it in a text file
-	search_word(search_word=args.word, file_path=args.filename)
-
-
-
-	
-	
-
-	
-	
-
+    # download the data and save it in a text file
+    search_word(search_word=args.word, file_path=args.filename)
